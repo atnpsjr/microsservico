d6ef2f1865f8e6803cf099f23e595adefa28e3d0
Antonio_Jr <antoniopsjr@usp.br>
2025-04-04 11:37:10 -0300

    projeto finalizado

---
+++ src/main/java/com/desafiobtg/orderms/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.desafiobtg.orderms.controller;
+
+import com.desafiobtg.orderms.controller.dto.ApiResponse;
+import com.desafiobtg.orderms.controller.dto.OrderResponse;
+import com.desafiobtg.orderms.controller.dto.PaginationResponse;
+import com.desafiobtg.orderms.service.OrderService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/customers/{customerId}/orders")
+    public ResponseEntity<ApiResponse<OrderResponse>> listOrders(@PathVariable("customerId") Long customerId,
+                                                                 @RequestParam(name = "page", defaultValue = "0") Integer page,
+                                                                 @RequestParam(name = "pageSize",defaultValue = "10") Integer pageSize){
+
+        var pageResponse = orderService.findAllCustomerId(customerId, PageRequest.of(page,pageSize));
+        var totalOnOrders = orderService.findTotalOnOrdersByCustomerId(customerId);
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                Map.of("totalonOrders", totalOnOrders),
+                pageResponse.getContent(),
+                PaginationResponse.fromPage(pageResponse)
+
+        ));
+    }
+}
---
+++ src/main/java/com/desafiobtg/orderms/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.desafiobtg.orderms.repository;
+
+import com.desafiobtg.orderms.controller.dto.OrderResponse;
+import com.desafiobtg.orderms.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OrderRepository extends MongoRepository<OrderEntity,Long> {
+    Page<OrderEntity> findAllByCustomerId(Long customerId, PageRequest pageRequest);
+}
